3ac0544a32b6c4f5053e9a65cc9f28357ff1f997
xiaxin <xia.xin.1988.07.28@163.com>
2020-03-20 12:44:49 +0800

    chinese-poetry init

---
+++ main_poetry.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import six, os, io
+from zhconv import convert_for_mw
+
+
+if six.PY2:
+    import sys
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+
+POETRY_DIRECTORY = './chinese-poetry/json/'
+
+def trans(name):
+
+    file_path = os.path.join(POETRY_DIRECTORY, name)
+
+    if os.path.isdir(file_path):
+        print file_path + " is dir"
+        return
+    else:
+        print file_path + " is file"
+
+    raw = io.open(file_path, 'r', encoding='utf-8').read()
+
+    if six.PY2:
+        content = convert_for_mw(unicode(raw), 'zh-cn')
+    else:
+        content = convert_for_mw(raw, 'zh-cn')
+
+    output_path = os.path.join('./poetry/', name)
+
+    with open(output_path, 'w') as f:
+        f.write(content)
+    
+    
+
+
+list(map(trans, os.listdir(POETRY_DIRECTORY)))
